d6b685fa655f98e8413627a0cfcbbb15063d611a
manlikleo <olaotan104@gmail.com>
2021-03-31 23:37:22 +0300

    compledted application

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
  let min = 1,
     max = 10,
-    winningNumber = 2,
+    winningNumber = getWinningNum(min,max),
     guesssesLeft = 3;
  
  
@@ -12,34 +12,62 @@ let game = document.querySelector('#game'),
     guessbtn = document.querySelector('#guess_value'),
     guessMessage = document.querySelector('.guess-message');
 
+    // event delegation is done when thw dom is already loaded and one needs to assign an event to a particular element.
+    game.addEventListener('mousedown', e=>{
+        if(e.target.className === 'play-again'){
+           window.location.reload();
+        }
+    });
+
  
  document.addEventListener('DOMContentLoaded', function (){
+
+
+  
+
     
     UImin.textContent = min;
     UImax.textContent = max;
 
 
     guessbtn.addEventListener('click', e=>{
+
         let guessNum = parseInt(guessInput.value);
         
         if (isNaN(guessNum) || guessNum < min || guessNum > max){
-            
-            message(`Please enter a number between ${min} and ${max}`,'red')
-        }
 
-        if(guessNum === winningNumber){
-            message(`${winningNumber} is correct, You Win !!`, 'green');
-            guessInput.disabled = true;
-            guessInput.style.border = ".5px solid green";
+            gameStatus(false,`Please enter a number between ${min} and ${max}`);
+            guessInput.disabled = false;
+            guessbtn.value = 'Submit';
+            guessbtn.className = '';
+
         }else{
-            if(! isNaN(guessNum) || !guessNum < min || !guessNum > max){
-                message(`You can try again !!`, 'red');
-                guessInput.style.border = ".5px solid red";
+
+            if(guessNum === winningNumber){
+                
+                gameStatus(true,`${winningNumber} is correct, You Win !!`)
+                
+    
+            }else{
+                    guesssesLeft -= 1;     
+                   
+                    if(guesssesLeft === 0){
+                        gameStatus(false,`Game Over, You lost,the correct number was ${winningNumber}`);       
+                    }else{
+                           
+                            gameStatus(false,`${guessNum} is not correct, ${guesssesLeft} guesses Left`);
+                            guessInput.disabled = false;
+                            guessbtn.value = 'Submit';
+                            guessbtn.className =''
+                    }
+               
+               
             }
            
         }
-        
-    })
+
+      
+    });
 
     guessInput.addEventListener('input',e =>{
         message('');
@@ -49,7 +77,33 @@ let game = document.querySelector('#game'),
  });
 
 
- function message(msg,color){
-     guessMessage.textContent = msg;
-     guessMessage.style.color = color;
+ 
+
+
+ function gameStatus(won,msg){
+
+    let confirmcolor;
+    // template literals
+    won === true ? confirmcolor = 'green' :  confirmcolor = 'red';
+  
+    guessInput.disabled = true;
+    guessInput.style.borderColor = confirmcolor;
+    message(msg,confirmcolor);
+
+    guessbtn.value = 'Play Again';
+    guessbtn.className += 'play-again';
  }
+
+ function message(msgt,color){
+    guessMessage.textContent = msgt;
+    guessMessage.style.color = color;
+}
+
+
+function getWinningNum(min,max){
+
+    let forrmcalu = (max-min+1) + min;
+    console.log(Math.floor(Math.random()*(forrmcalu)));
+    return Math.floor(Math.random()*(forrmcalu));
+        
+}
